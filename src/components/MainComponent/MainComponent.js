6c3261e8999f1049de4fd5278d86044063f60590
Patricia Garcia <patijoe33@gmail.com>
2020-05-09 11:15:31 +0200

    modify styles

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,85 +1,29 @@
-import React from 'react';
-import HeroComponent from '../HeroComponent/index';
-import car from './images/pill-image.jpg';
+import React from "react";
+import HeroComponent from "../HeroComponent/index";
+import CarCardComponent from "../CarCardComponent/index";
+import carsInfo from "../../constants/carsCard";
 
 class MainComponent extends React.Component {
   render() {
- 
-
     return (
-      <div>
+      <div className="main-container">
         <HeroComponent />
-        <div className="container"></div>
-        <div className="row">
-      <div className="cc_section_title">
-        <h2 className="titulo">Ofertas destacadas de seminuevos y km 0</h2>
-        <p>Seleccionamos las mejores ofertas de coches seminuevos según segmento y precio de mercado del vehículo</p>
-      </div>
-      <div className="cc_slider_enviroment">
-        <div className="cc_pills_container">
-          <div id="carCard" className="cc_carCard">
-            <div className="cc_card_photo">
-              <label className="cc_label cc_km0">
-                <span>km 0</span>
-              </label>
-              <img src={car} className="img-responsive" alt="Renault Scénic 1.3 TCe GPF Zen 103kW - 21.490 € - coches.com" />
-            </div>
-            <div className="cc_card_data">
-              <p>Renault Scénic</p>
-              <p>Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.</p>
-              <p className="cc_precio">21.490 €</p>
-              <p className="cc_line_height">Esta grand scénic 1.3 tce gpf zen 103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra...</p>
-            </div>
-            <div className="cc_card_data_tech">
-              <p>140 cv</p>
-              <p>9.850 km</p>
-              <p>2019</p>
-              <p>Cadiz</p>
-            </div>
-          </div>
-          <div id="carCard" className="cc_carCard">
-            <div className="cc_card_photo">
-              <label className="cc_label cc_km0">
-                <span>km 0</span>
-              </label>
-              <img src={car} className="img-responsive" alt="Renault Scénic 1.3 TCe GPF Zen 103kW - 21.490 € - coches.com" />
-            </div>
-            <div className="cc_card_data">
-              <p>Renault Scénic</p>
-              <p>Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.</p><p className="cc_precio">21.490 €</p>
-              <p className="cc_line_height">Esta grand scénic 1.3 tce gpf zen 103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra...</p>
-            </div>
-            <div className="cc_card_data_tech">
-              <p>140 cv</p>
-              <p>9.850 km</p>
-              <p>2019</p>
-              <p>Cadiz</p>
-            </div>
-          </div>
-          <div id="carCard" className="cc_carCard">
-            <div className="cc_card_photo">
-              <label className="cc_label cc_km0">
-                <span>km 0</span>
-              </label>
-              <img src={car} className="img-responsive" alt="Renault Scénic 1.3 TCe GPF Zen 103kW - 21.490 € - coches.com" />
-            </div>
-            <div className="cc_card_data">
-              <p>Renault Scénic</p>
-              <p>Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.</p>
-              <p className="cc_precio">21.490 €</p>
-              <p className="cc_line_height">Esta grand scénic 1.3 tce gpf zen  103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra...</p>
-            </div>
-            <div className="cc_card_data_tech">
-              <p>140 cv</p>
-              <p>9.850 km</p>
-              <p>2019</p>
-              <p>Cadiz</p>
-            </div>
-          </div>
+        <div className="main-section">
+          <h2 className="main-section_title">
+            <strong>15.531 ofertas</strong> de coches
+            <strong> seminuevos</strong> y de <strong>Km0</strong>
+          </h2>
+          <ul className="main-slider_list">
+            {carsInfo.map((item, index) => {
+              return (
+                <li className="card-slider_item" key={index}>
+                  <CarCardComponent item={item} />
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
-    </div>
-  </div>
     );
   }
 }
---
+++ src/constants/carsCard.js
@@ -0,0 +1,52 @@
+const carsInfo = [
+  {
+    type: "Km 0",
+    generalData: {
+      name: "Renault Scénic",
+      model: "Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.",
+      price: "21.490 €",
+      characteristics:
+        "Esta grand scénic 1.3 tce gpf zen 103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra..."
+    },
+    techData: {
+      capacity: "140 cv",
+      kms: "9.850 km",
+      year: "2019",
+      from: "Cadiz"
+    }
+  },
+  {
+    type: "Km 0",
+    generalData: {
+      name: "Renault Scénic",
+      model: "Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.",
+      price: "21.490 €",
+      characteristics:
+        "Esta grand scénic 1.3 tce gpf zen 103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra..."
+    },
+    techData: {
+      capacity: "140 cv",
+      kms: "9.850 km",
+      year: "2019",
+      from: "Cadiz"
+    }
+  },
+  {
+    type: "Km 0",
+    generalData: {
+      name: "Renault Scénic",
+      model: "Scénic 1.3 TCe GPF 1.3 TCe GPF Zen 5p.",
+      price: "21.490 €",
+      characteristics:
+        "Esta grand scénic 1.3 tce gpf zen 103kw 140cv se caracteriza muy bien por poder albergar tanto a pasajeros como toda nuestra..."
+    },
+    techData: {
+      capacity: "140 cv",
+      kms: "9.850 km",
+      year: "2019",
+      from: "Cadiz"
+    }
+  }
+];
+
+export default carsInfo;
