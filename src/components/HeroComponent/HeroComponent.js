5bf8ab8be9e002e6068c7cb44e0d977b5e5d4478
Patricia Garcia <patijoe33@gmail.com>
2020-05-09 16:20:58 +0200

    format some files

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import FormComponent from "../FormComponent/index";
 
 class HeroComponent extends React.Component {
   render() {
- 
-
     return (
-        <div id="hero" className="hero-container">
-          <div className="hero-title">
-              <h1>La mayor oferta de coches Seminuevos y km 0 </h1>
-          </div>
-          <FormComponent />
+      <div id="hero" className="hero-container">
+        <div className="hero-title">
+          <h1>La mayor oferta de coches Seminuevos y km 0 </h1>
         </div>
+        <FormComponent />
+      </div>
     );
   }
 }
